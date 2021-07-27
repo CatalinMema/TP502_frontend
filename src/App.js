009034e1ee4f65edd6052af8d6ac7598011e0edf
catalin.mema@titeica.com <catalin.mema@titeica.com>
2021-07-27 11:52:02 +0300

    feat: add SignUp and SignIn pages

--- src/App.js
+++ src/App.js
@@ -7,17 +7,26 @@ import {
   Route,
 } from "react-router-dom";
 import Recipes from './components/Recipes/Recipes';
+import SignIn from './components/SignIn/SignIn';
+import SignUp from './components/SignUp/SignUp';
 function App() { 
   return (
     <Router>
     <div className="App">
-         <Header />
     <Switch>
         <Route exact path="/">
+        <Header />
           <Home />
         </Route>
         <Route exact path="/recipes">
-         <Recipes />
+        <Header />
+        <Recipes />
+        </Route>
+        <Route exact path="/signin">
+          <SignIn/>
+        </Route>
+        <Route exact path="/signup">
+          <SignUp/>
         </Route>
    </Switch>
     </div>
